6e7ed591bd2497aa0f864dbecb542a719e54f86b
ariel7342 <ariell@wix.com>
2020-11-20 08:22:21 +0200

    moved functions declaration up top

--- main.c
+++ main.c
@@ -10,104 +10,17 @@
 #define SPRITE_OR_COLA_PRICE 4
 
 /* get change amount and prints the change amount in coins view*/
-void printChangeCoins(int changeAmount) {
-    int oneCoinsAmount = 0, twoCoinsAmount = 0, fiveCoinsAmount = 0, tenCoinsAmount = 0;
-    int sum = 0;
-
-    if (changeAmount == 0) {
-        printf("Thank you for buying!\n");
-    } else {
-        while (sum < changeAmount) {
-            if (sum + 10 <= changeAmount) {
-                tenCoinsAmount++;
-                sum += 10;
-            } else if (sum + 5 <= changeAmount) {
-                fiveCoinsAmount++;
-                sum += 5;
-            } else if (sum + 2 <= changeAmount) {
-                twoCoinsAmount++;
-                sum += 2;
-            } else if (sum + 1 <= changeAmount) {
-                oneCoinsAmount++;
-                sum += 1;
-            }
-        }
-        printf("You get back (1, 2, 5, 10): %d %d %d %d\nThank you for buying!\n", oneCoinsAmount, twoCoinsAmount,
-               fiveCoinsAmount, tenCoinsAmount);
-    }
-}
+void printChangeCoins(int changeAmount);
 
 /*get coins and return amount in number*/
 int getTotalCoinsAmount(int oneCoinsAmount, int twoCoinsAmount,
-                        int fiveCoinsAmount, int tenCoinsAmount) {
-    int totalAmount = 0;
-    /*checks if oneCoinsAmount bigger then 0 and count how many*/
-    if (oneCoinsAmount > 0) {
-        for (int i = 0; i < oneCoinsAmount; ++i) {
-            totalAmount++;
-        }
-    }
-
-    /*checks if twoCoinsAmount bigger then 0 and count how many*/
-    if (twoCoinsAmount > 0) {
-        for (int i = 0; i < twoCoinsAmount; ++i) {
-            totalAmount += 2;
-        }
-    }
-
-    /*checks if fiveCoinsAmount bigger then 0 and count how many*/
-    if (fiveCoinsAmount > 0) {
-        for (int i = 0; i < fiveCoinsAmount; ++i) {
-            totalAmount += 5;
-        }
-    }
-
-    /*checks if tenCoinsAmount bigger then 0 and count how many*/
-    if (tenCoinsAmount > 0) {
-        for (int i = 0; i < tenCoinsAmount; ++i) {
-            totalAmount += 10;
-        }
-    }
-
-    return totalAmount;
-}
+                        int fiveCoinsAmount, int tenCoinsAmount);
 
 /*calc the change amount*/
-int getChangeAmount(int currentPrice, int totalAmount) {
-    return (totalAmount - currentPrice);
-}
+int getChangeAmount(int currentPrice, int totalAmount);
 
 /* get current bottle price and handles payment process*/
-void pay(int currentPrice) {
-    int oneCoinsAmount = 0, twoCoinsAmount = 0, fiveCoinsAmount = 0, tenCoinsAmount = 0, totalAmount = 0;
-    bool isPaymentProcessFinished = false;
-
-    int changeAmount;
-
-    printf("The price is %d nis.\n", currentPrice);
-    while (!isPaymentProcessFinished) {
-        printf("To pay please enter 4 numbers. The first number is the amount of\n"
-               "1 nis coins, the second is the amount of 2 nis coins, then 5 and 10\n"
-               "Please enter payment (1, 2, 5, 10):\n");
-        scanf("%d %d %d %d", &oneCoinsAmount, &twoCoinsAmount, &fiveCoinsAmount, &tenCoinsAmount);
-
-        totalAmount += getTotalCoinsAmount(oneCoinsAmount, twoCoinsAmount,
-                                           fiveCoinsAmount, tenCoinsAmount);
-        changeAmount = getChangeAmount(currentPrice, totalAmount);
-
-        /*check if there change*/
-        if (changeAmount > 0) {
-            isPaymentProcessFinished = true;
-            printf("Your change is: %d nis.\n", changeAmount);
-            printChangeCoins(changeAmount);
-        } else if (changeAmount == 0) {
-            isPaymentProcessFinished = true;
-            printChangeCoins(changeAmount);
-        } else {
-            printf("The amount does not suffice! Please enter %d more nis.\n", -changeAmount);
-        }
-    }
-}
+void pay(int currentPrice);
 
 /*This is a drink machine code.*/
 /*It prints drink choices and get number as input from user selection.*/
@@ -118,9 +31,6 @@ void pay(int currentPrice) {
 void main() {
     int drinkChoice = 0, currentPrice;
     int amountOfWaterBottles = 10, amountOfOrangeBottles = 10, amountOfAppleBottles = 10, amountOfColaBottles = 10, amountOfSpriteBottles = 10;
-
-    bool isValid = true;
-    bool isExit = false;
 
     while (drinkChoice != 6) {
         printf("Please choose:\n1. Water.\n2. Orange juice.\n3. Apple juice.\n4. Sprite.\n5. Cola.\n6. exit.\n");
@@ -174,12 +84,106 @@ void main() {
                 }
                 break;
             case 6:
-                isExit = true;
                 printf("Good bye.\n");
                 break;
             default:
-                isValid = false;
                 printf("Invalid choice.\n");
+        }
+    }
+}
+
+void printChangeCoins(int changeAmount) {
+    int oneCoinsAmount = 0, twoCoinsAmount = 0, fiveCoinsAmount = 0, tenCoinsAmount = 0;
+    int sum = 0;
+
+    if (changeAmount == 0) {
+        printf("Thank you for buying!\n");
+    } else {
+        while (sum < changeAmount) {
+            if (sum + 10 <= changeAmount) {
+                tenCoinsAmount++;
+                sum += 10;
+            } else if (sum + 5 <= changeAmount) {
+                fiveCoinsAmount++;
+                sum += 5;
+            } else if (sum + 2 <= changeAmount) {
+                twoCoinsAmount++;
+                sum += 2;
+            } else if (sum + 1 <= changeAmount) {
+                oneCoinsAmount++;
+                sum += 1;
+            }
+        }
+        printf("You get back (1, 2, 5, 10): %d %d %d %d\nThank you for buying!\n", oneCoinsAmount, twoCoinsAmount,
+               fiveCoinsAmount, tenCoinsAmount);
+    }
+}
+
+int getTotalCoinsAmount(int oneCoinsAmount, int twoCoinsAmount,
+                        int fiveCoinsAmount, int tenCoinsAmount) {
+    int totalAmount = 0;
+    /*checks if oneCoinsAmount bigger then 0 and count how many*/
+    if (oneCoinsAmount > 0) {
+        for (int i = 0; i < oneCoinsAmount; ++i) {
+            totalAmount++;
+        }
+    }
+
+    /*checks if twoCoinsAmount bigger then 0 and count how many*/
+    if (twoCoinsAmount > 0) {
+        for (int i = 0; i < twoCoinsAmount; ++i) {
+            totalAmount += 2;
+        }
+    }
+
+    /*checks if fiveCoinsAmount bigger then 0 and count how many*/
+    if (fiveCoinsAmount > 0) {
+        for (int i = 0; i < fiveCoinsAmount; ++i) {
+            totalAmount += 5;
+        }
+    }
+
+    /*checks if tenCoinsAmount bigger then 0 and count how many*/
+    if (tenCoinsAmount > 0) {
+        for (int i = 0; i < tenCoinsAmount; ++i) {
+            totalAmount += 10;
+        }
+    }
+
+    return totalAmount;
+}
+
+int getChangeAmount(int currentPrice, int totalAmount) {
+    return (totalAmount - currentPrice);
+}
+
+void pay(int currentPrice) {
+    int oneCoinsAmount = 0, twoCoinsAmount = 0, fiveCoinsAmount = 0, tenCoinsAmount = 0, totalAmount = 0;
+    bool isPaymentProcessFinished = false;
+
+    int changeAmount;
+
+    printf("The price is %d nis.\n", currentPrice);
+    while (!isPaymentProcessFinished) {
+        printf("To pay please enter 4 numbers. The first number is the amount of\n"
+               "1 nis coins, the second is the amount of 2 nis coins, then 5 and 10\n"
+               "Please enter payment (1, 2, 5, 10):\n");
+        scanf("%d %d %d %d", &oneCoinsAmount, &twoCoinsAmount, &fiveCoinsAmount, &tenCoinsAmount);
+
+        totalAmount += getTotalCoinsAmount(oneCoinsAmount, twoCoinsAmount,
+                                           fiveCoinsAmount, tenCoinsAmount);
+        changeAmount = getChangeAmount(currentPrice, totalAmount);
+
+        /*check if there change*/
+        if (changeAmount > 0) {
+            isPaymentProcessFinished = true;
+            printf("Your change is: %d nis.\n", changeAmount);
+            printChangeCoins(changeAmount);
+        } else if (changeAmount == 0) {
+            isPaymentProcessFinished = true;
+            printChangeCoins(changeAmount);
+        } else {
+            printf("The amount does not suffice! Please enter %d more nis.\n", -changeAmount);
         }
     }
 }
